b2bf33921421ecce8b05d51f88518fb6fee9acdc
shashanksh8e <shashank648a@gmail.com>
2026-01-09 23:29:54 +0530

    Simplify for Railway deployment

--- server.js
+++ server.js
@@ -140,13 +140,11 @@ app.delete('/api/contacts/:id', async (req, res) => {
 });
 
 // Serve static files from React build
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, 'client/build')));
-  
-  app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
-  });
-}
+app.use(express.static(path.join(__dirname, 'client/build')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+});
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
